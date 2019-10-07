d95d08551cb7aea1f2e0f8f71a0ebc60900b64e1
ulukfuni <vtn1989@gmail.com>
2019-10-07 13:39:10 -0400

    the start of the now page

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
       },
     },
     {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            path: `${__dirname}/content/now`,
+            name: `now`,
+        }
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
